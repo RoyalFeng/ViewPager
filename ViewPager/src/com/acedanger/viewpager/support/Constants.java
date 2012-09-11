c381b1d11382c08c34dc8c376e3d2beaa53b197f
Pete Wood <acedanger49@gmail.com>
2012-09-11 18:24:23 -0400

    Updated path

--- ViewPager/src/com/acedanger/viewpager/support/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.acedanger.viewpager.support;
-
-public class Constants {
-	public static final String SORT_BY_DATE = "date";
-	public static final String SORT_BY_DESCRIPTION = "description";
-}
