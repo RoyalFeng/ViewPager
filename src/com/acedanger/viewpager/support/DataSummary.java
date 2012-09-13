646ab0176418e9dc82e473d9bc7002187592f9da
Pete Wood <acedanger49@gmail.com>
2012-09-13 09:23:25 -0400

    made the data summary POJO parcelable.

--- src/com/acedanger/viewpager/support/DataSummary.java
+++ src/com/acedanger/viewpager/support/DataSummary.java
@@ -1,6 +1,9 @@
 package com.acedanger.viewpager.support;
 
-public class DataSummary {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class DataSummary implements Parcelable {
 	public String year;
 	public String month;
 	public String count;
@@ -29,4 +32,34 @@ public class DataSummary {
 		this.count = count;
 	}
 
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel parcel, int flags) {
+		parcel.writeString(year);
+		parcel.writeString(month);
+		parcel.writeString(count);
+	}
+	
+	public DataSummary() {
+	}
+	
+	private DataSummary(Parcel in) {
+		this.year = in.readString();
+		this.month = in.readString();
+		this.count = in.readString();
+	}
+	
+	public static final Parcelable.Creator<DataSummary> CREATOR = new Parcelable.Creator<DataSummary>() {
+		public DataSummary createFromParcel(Parcel in) {
+			return new DataSummary(in);
+		}
+
+		public DataSummary[] newArray(int size) {
+			return new DataSummary[size];
+		}
+	};
 }
